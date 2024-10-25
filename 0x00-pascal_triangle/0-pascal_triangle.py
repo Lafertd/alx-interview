9725032acc6afecb5b293c8118d35ba5dc9c5b94
Lafertd <43250553+Lafertd@users.noreply.github.com>
2024-10-25 01:51:08 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,7 +3,8 @@ from math import factorial
 
 def pascal_triangle(n):
     fact = factorial
-
+if <= 0:
+    return []
 
 # Generate each row of Pascal's Triangle
 row = [fact(m) // (fact(k) * fact(m - k)) for k in range(m + 1)]
